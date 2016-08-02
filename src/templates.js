9dc17973bf2599e4457f666943d052f3ec6d204a
amorgunov <aleksandr.morgunov@noveogroup.com>
2016-08-02 15:36:30 +0700

    small fix;

--- src/templates.js
+++ src/templates.js
@@ -1,17 +1,18 @@
-
-
-export const TaskTemplate = `
+const TaskTemplate = `
   <%= title %> <!--span>(<%= sort %>)</span-->
   <button class="edit">Edit</button> 
   <button class="delete">Delete</button>
   `;
 
-export const HomeTemplate = `
+const HomeTemplate = `
     <h1>Home</h1>
     <a href="#about">Go to "About"</a>
   `;
 
-export const AboutTemplate = `
+const AboutTemplate = `
     <h1>About</h1>
     <a href="#">Go to back to "Home"</a>
   `;
+
+
+export { TaskTemplate, HomeTemplate, AboutTemplate };
